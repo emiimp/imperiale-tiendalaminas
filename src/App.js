1d1eaf74d999008a467b88de36f92f0d156bf553
emiimp <emiliadasmode@gmail.com>
2022-09-06 22:20:59 -0300

    desafio contador con boton

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import './App.css';
 import './Components.css';
 import NavBar from './Components/NavBar/NavBar';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import Counter from './Components/Counter/Counter.js';
+
 
 function App() {
+
+  const stock = 3;
+
   return (
     <div className="App">
+
       <NavBar/>
+
       <ItemListContainer greeting ="¡Bienvenido!"/>
+
+      <Counter stock={stock}/>
+
       <header className="App-header">
       </header>
+      
     </div>
   );
 };
